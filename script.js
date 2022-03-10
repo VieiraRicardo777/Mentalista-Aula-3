75ff541dd89ce5bc2ad121284862f65ad2459ffa
Ricardo <khricardo@gmail.com>
2022-03-10 09:08:33 -0300

    Master

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var numeroSecreto = parseInt(Math.random() * 11);
 var tentativas = 0;
+var opcao = document.getElementById("jogar").value;
 var elementoRasultado = document.getElementById("resultado");
 var chute = document.getElementById("valor");
 
@@ -9,27 +10,24 @@ function Chutar() {
 
 	if (tentativas == 3) {
 		elementoResultado.innerHTML =
-		  "Você tentou 3 vezes e não acertou! Mas tente novamente! O número era " +
-		  numeroSecreto;
+			"Você tentou 3 vezes e não acertou! Mas tente novamente! O número era " +
+			numeroSecreto;
 	} else {
-	if (chute == numeroSecreto) {
-		tentativas += 1 
-		elementoResultado.innerHTML = "Você Acertou Miserávi " + tentativas + "tentativas.";
-    } else if (chute > 10 || chute < 0) {
-		elementoResultado.innerHTML = "Tentativas inválidas!";
-
-	} else {
-		if (numeroSecreto || chute && tentativas < 3) {
-		  elementoResultado.innerHTML =
-			"Você errou. Tente mais uma vez!";	
-	} 
-	tentativas += 1;
+		if (chute == numeroSecreto) {
+			tentativas += 1;
+			elementoResultado.innerHTML =
+				"Você Acertou Miserávi" + tentativas + " tentativas.";
+		} else if (chute > 10 || chute < 0) {
+			elementoResultado.innerHTML = "Tentativas inválidas!";
+		} else {
+			if (numeroSecreto || (chute && tentativas < 3)) {
+				elementoResultado.innerHTML = "Você errou. Tente mais uma vez!";
+			}
 			if (tentativas >= 3) {
-			  elementoResultado.innerHTML =
-				"Você errou 3 vezes. Que pena! O número era " +
-				numeroSecreto;		  
+				elementoResultado.innerHTML =
+					"Você errou 3 vezes. Que pena! O número era " + numeroSecreto;
+				}
 			}
 		}
 	}
-		
 }
